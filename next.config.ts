5aa38484058fc8a09e2868ab828aaede2b207d90
David <david.kjellstrand@gmail.com>
2026-09-02 11:41:10 +0200

    fix/redirect to new domain

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "davidkjellstrand-portfolio.vercel.app",
+          },
+        ],
+        destination: "https://www.davidkjellstrand.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
